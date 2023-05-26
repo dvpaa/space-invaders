565ac1d59fceec246f5d81158b6be3a86c64573c
dvpaa <kgo000926@gmail.com>
2023-05-26 13:04:08 +0900

    refactor: separate run application

---
+++ src/main/java/org/newdawn/spaceinvaders/GameApplication.java
@@ -0,0 +1,9 @@
+package org.newdawn.spaceinvaders;
+
+import org.newdawn.spaceinvaders.frame.MainFrame;
+
+public class GameApplication {
+	public static void main(String[] args) {
+		new MainFrame();
+	}
+}
